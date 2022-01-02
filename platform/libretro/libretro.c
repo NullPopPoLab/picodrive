bff8b96edd7895623fee5ad274a59ce13b4a65c9
sylph@reset.jp <NullPopPo>
2022-01-02 14:26:33 +0000

    retro_pico_map 添字とkey IDは無関係とする

--- platform/libretro/libretro.c
+++ platform/libretro/libretro.c
@@ -1399,19 +1399,24 @@ void retro_reset(void)
    PicoReset();
 }
 
-static const unsigned short retro_pico_map[] = {
-   [RETRO_DEVICE_ID_JOYPAD_UP]     = 1 << GBTN_UP,
-   [RETRO_DEVICE_ID_JOYPAD_DOWN]   = 1 << GBTN_DOWN,
-   [RETRO_DEVICE_ID_JOYPAD_LEFT]   = 1 << GBTN_LEFT,
-   [RETRO_DEVICE_ID_JOYPAD_RIGHT]  = 1 << GBTN_RIGHT,
-   [RETRO_DEVICE_ID_JOYPAD_C]      = 1 << GBTN_A,
-   [RETRO_DEVICE_ID_JOYPAD_B]      = 1 << GBTN_B,
-   [RETRO_DEVICE_ID_JOYPAD_A]      = 1 << GBTN_C,
-   [RETRO_DEVICE_ID_JOYPAD_Z]      = 1 << GBTN_X,
-   [RETRO_DEVICE_ID_JOYPAD_Y]      = 1 << GBTN_Y,
-   [RETRO_DEVICE_ID_JOYPAD_X]      = 1 << GBTN_Z,
-   [RETRO_DEVICE_ID_JOYPAD_R]      = 1 << GBTN_MODE,
-   [RETRO_DEVICE_ID_JOYPAD_START]  = 1 << GBTN_START,
+typedef struct {
+	unsigned short retro;
+	unsigned short pico;
+}retro_pico_assign;
+
+static const retro_pico_assign retro_pico_map[] = {
+   {RETRO_DEVICE_ID_JOYPAD_UP, 1 << GBTN_UP},
+   {RETRO_DEVICE_ID_JOYPAD_DOWN, 1 << GBTN_DOWN},
+   {RETRO_DEVICE_ID_JOYPAD_LEFT, 1 << GBTN_LEFT},
+   {RETRO_DEVICE_ID_JOYPAD_RIGHT, 1 << GBTN_RIGHT},
+   {RETRO_DEVICE_ID_JOYPAD_C, 1 << GBTN_A},
+   {RETRO_DEVICE_ID_JOYPAD_B, 1 << GBTN_B},
+   {RETRO_DEVICE_ID_JOYPAD_A, 1 << GBTN_C},
+   {RETRO_DEVICE_ID_JOYPAD_Z, 1 << GBTN_X},
+   {RETRO_DEVICE_ID_JOYPAD_Y, 1 << GBTN_Y},
+   {RETRO_DEVICE_ID_JOYPAD_X, 1 << GBTN_Z},
+   {RETRO_DEVICE_ID_JOYPAD_R, 1 << GBTN_MODE},
+   {RETRO_DEVICE_ID_JOYPAD_START, 1 << GBTN_START},
 };
 #define RETRO_PICO_MAP_LEN (sizeof(retro_pico_map) / sizeof(retro_pico_map[0]))
 
@@ -1703,12 +1708,12 @@ void retro_run(void)
       if (libretro_supports_bitmasks) {
          input = input_state_cb(pad, RETRO_DEVICE_JOYPAD, 0, RETRO_DEVICE_ID_JOYPAD_MASK);
          for (i = 0; i < RETRO_PICO_MAP_LEN; i++)
-            if (input & (1 << i))
-               PicoIn.pad[pad] |= retro_pico_map[i];
+            if (input & (1<<retro_pico_map[i].retro))
+               PicoIn.pad[pad] |= retro_pico_map[i].pico;
       } else {
          for (i = 0; i < RETRO_PICO_MAP_LEN; i++)
-            if (input_state_cb(pad, RETRO_DEVICE_JOYPAD, 0, i))
-               PicoIn.pad[pad] |= retro_pico_map[i];
+            if (input_state_cb(pad, RETRO_DEVICE_JOYPAD, 0, retro_pico_map[i].retro))
+               PicoIn.pad[pad] |= retro_pico_map[i].pico;
       }
    }
 
